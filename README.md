b0d17370b15fda0c5bcb7d7edcdcd6fedec44a4e
ramantv <90482664+ramantv@users.noreply.github.com>
2021-11-28 21:15:51 -0600

    Fixed prompting bugs, updated validation methods and added descriptive comments. Generated and updated Readme

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# rvt_team_profile_generator
+# Team Profile Generator 
+> A command line application that takes user input and generates an HTML page containing the profile of a software engineering team
+
+## Table of contents
+  - [General info](#general-info)
+  - [Screenshots](#screenshots)
+  - [Video Demo](#video-demo)
+  - [Licenses](#licenses)
+  - [Setup](#setup)
+  - [Code](#code)
+  - [Developer(s)](#developers)
+
+## General info
+This application will create a software engineering team's profile with all team members names and basic employee details including email addresses and links to their Github profiles. User will enter the manager's information first, then enter the details of all other team members including all engineers and interns. The application is written using JavaScript and should be run using the Node.js JavaScript runtime environment. The NPM inquirer package is used to provide the command line interface to gather user input, and the jest package is used to validate classes.
+
+## Screenshots
+![Example Portfolio](./images/rvt_team_profile.png)
+
+## Video Demo
+[Team Portfolio Demo](https://www.youtube.com/watch?v=ZMrtMJ6bMuM)
+
+## Licenses
+![License](https://img.shields.io/static/v1?label=License&message=MIT&color=BLUE) 
+![License](https://img.shields.io/static/v1?label=License&message=ISC&color=BLUE) 
+
+## Setup
+* run these commands in order:
+   1. npm install
+   2. node index.js
+
+## Code
+* HTML/CSS
+* JavaScript
+* Bootstrap
+* Node.js
+* Jest
+* Inquirer
+* fs
+
+## Developer(s)
+* Raman TV.
--- index.js
+++ index.js
@@ -1,49 +1,15 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 
-const generatePage = require("./src/page-template");
+const generateTeamProfile = require("./src/page-template");
 
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
-const writeFile = (fileContent) => {
-  return new Promise((resolve, reject) => {
-    fs.writeFile("./dist/index.html", fileContent, (err) => {
-      // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
-      if (err) {
-        reject(err);
-        // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
-        return;
-      }
-
-      // if everything went well, resolve the Promise and send the successful data to the `.then()` method
-      resolve({
-        ok: true,
-        message: "Team profile created!",
-      });
-    });
-  });
-};
-
-const copyFile = () => {
-  return new Promise((resolve, reject) => {
-    fs.copyFile("./src/style.css", "./dist/style.css", (err) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-
-      resolve({
-        ok: true,
-        message: "Check out the generated team profile page at ./dist/index.html",
-      });
-    });
-  });
-};
-
-const promptManager = () => {
-    var teamData = [];
+// function to prompt user to enter Manager details
+const getManagerDetails = () => {
+  var teamData = [];
   return inquirer
     .prompt([
       {
@@ -55,13 +21,13 @@ const promptManager = () => {
         type: "input",
         name: "id",
         message: "What is the manager's ID?",
-        validate: validateEmployeeID
+        validate: validateEmployeeID,
       },
       {
         type: "input",
         name: "email",
         message: "What is the manager's email?",
-        validate: validateEmail
+        validate: validateEmail,
       },
       {
         type: "input",
@@ -77,7 +43,8 @@ const promptManager = () => {
     });
 };
 
-const promptEmployee = (teamData) => {
+// function to prompt user to get Employee details. Recursively calls itself until the user is finished entering all employee/intern details.
+const getEmployeeDetails = (teamData) => {
   return inquirer
     .prompt([
       {
@@ -101,13 +68,13 @@ const promptEmployee = (teamData) => {
               type: "input",
               name: "id",
               message: "What is the engineer's ID number?",
-              validate: validateEmployeeID
+              validate: validateEmployeeID,
             },
             {
               type: "input",
               name: "email",
               message: "What is the engineer's email?",
-              validate: validateEmail
+              validate: validateEmail,
             },
             {
               type: "input",
@@ -123,7 +90,7 @@ const promptEmployee = (teamData) => {
               employeeData.github
             );
             teamData.push(employee);
-            return promptEmployee(teamData);
+            return getEmployeeDetails(teamData);
           });
       } else if (role === "Intern") {
         return inquirer
@@ -137,13 +104,13 @@ const promptEmployee = (teamData) => {
               type: "input",
               name: "id",
               message: "What is the intern's ID number?",
-              validate: validateEmployeeID
+              validate: validateEmployeeID,
             },
             {
               type: "input",
               name: "email",
               message: "What is the intern's email?",
-              validate: validateEmail
+              validate: validateEmail,
             },
             {
               type: "input",
@@ -159,7 +126,7 @@ const promptEmployee = (teamData) => {
               employeeData.school
             );
             teamData.push(employee);
-            return promptEmployee(teamData);
+            return getEmployeeDetails(teamData);
           });
       } else {
         return teamData;
@@ -167,26 +134,68 @@ const promptEmployee = (teamData) => {
     });
 };
 
+// employeeID validator
 const validateEmployeeID = (empId) => {
-    const employeeIdRegex = /^[a-zA-Z0-9\-_]{1,10}$/;
-    if(!employeeIdRegex.test(empId)) {
-        return "Employee ID should be an alpha-numeric of maximum length up to 10 characters!";
-    }
-    return true;
-}
+  const employeeIdRegex = /^[a-zA-Z0-9\-_]{1,10}$/;
+  if (!employeeIdRegex.test(empId)) {
+    return "Employee ID should be an alpha-numeric of maximum length up to 10 characters!";
+  }
+  return true;
+};
 
+// email address validator
 const validateEmail = (email) => {
-    const emailRegEx = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
-    if(!emailRegEx.test(email)) {
-        return "Please enter a valid email address!";
-    }
-    return true;
-}
+  const emailRegEx =
+    /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+  if (!emailRegEx.test(email)) {
+    return "Please enter a valid email address!";
+  }
+  return true;
+};
 
-promptManager()
-  .then(promptEmployee)
+// function to write out the generated team profile HTML to the output file
+const writeFile = (fileContent) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile("./dist/index.html", fileContent, (err) => {
+      // if there's an error, reject the Promise and return
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      // if team profile write succeeded, resolve the Promise and send OK.
+      resolve({
+        ok: true,
+        message: "Team profile created!",
+      });
+    });
+  });
+};
+
+// function to copy the custom stylesheet file into the output folder 
+const copyStylesheet = () => {
+  return new Promise((resolve, reject) => {
+    fs.copyFile("./src/style.css", "./dist/style.css", (err) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve({
+        ok: true,
+        message:
+          "Check out the generated team profile page at ./dist/index.html",
+      });
+    });
+  });
+};
+
+
+// initiate the data collection and team profile generation.
+getManagerDetails()
+  .then(getEmployeeDetails)
   .then((teamData) => {
-    return generatePage(teamData);
+    return generateTeamProfile(teamData);
   })
   .then((pageHTML) => {
     return writeFile(pageHTML);
@@ -194,10 +203,10 @@ promptManager()
   .then((writeFileResponse) => {
     console.log("------------------------");
     console.log(writeFileResponse.message);
-    return copyFile();
+    return copyStylesheet();
   })
-  .then((copyFileResponse) => {
-    console.log(copyFileResponse.message);
+  .then((copyStylesResponse) => {
+    console.log(copyStylesResponse.message);
     console.log("------------------------");
   })
   .catch((err) => {
